7c03a245669dddcfe06740109203c0c175253280
Markus Bopp <uniftyio@gmail.com>
2023-03-29 01:08:52 +0200

    # tx speed up thanks to supertestnet's hints # new design based on bootstrap (dark mode next) # added brc-20 minting and deployment

---
+++ js/custom.js
@@ -0,0 +1,52 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-0;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 2,
+            },
+            767:{
+                items: 3,
+            },
+            1200:{
+                items: 4,
+            }
+        }
+    });
+  
+  })(window.jQuery);
+
+
