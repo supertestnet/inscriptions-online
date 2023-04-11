6210f048cba9410bb4d2829d630c03fb0ab9d5c4
Markus Bopp <uniftyio@gmail.com>
2023-04-11 09:11:48 +0200

    + added faster .unisat availability check (existence.php)

---
+++ existence.php
@@ -0,0 +1,24 @@
+<?php
+$url = "https://unisat.io/brc20-api/inscriptions/category/unisat/existence";
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$name = json_encode($_GET['text']);
+
+$data = '{"names":['.$name.']}';
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+$resp = curl_exec($curl);
+
+if($resp === false)
+{
+    echo 'error';
+}
+
+curl_close($curl);
+
+echo $resp;
