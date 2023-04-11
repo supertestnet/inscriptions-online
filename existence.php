3634426b434bbfda58faf0b1865c6afa7a371cfa
Markus Bopp <uniftyio@gmail.com>
2023-04-11 09:44:35 +0200

    + added isset() check on text parameter and exit if not set

--- existence.php
+++ existence.php
@@ -1,6 +1,12 @@
 <?php
 $url = "https://unisat.io/brc20-api/inscriptions/category/unisat/existence";
 
+if(!isset($_GET['text']))
+{
+    echo 'error';
+    exit;
+}
+
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
